1461db3654e63e5c158178b6f077607bc94f6d79
crosslife <geziyang@foxmail.com>
2014-03-14 18:24:45 +0800

    admob test

--- proj.android/src/org/cocos2dx/lua/Cocos2dxActivity.java
+++ proj.android/src/org/cocos2dx/lua/Cocos2dxActivity.java
@@ -1,32 +1,49 @@
 package org.cocos2dx.lua;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+
 import android.app.NativeActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.crosslife.FlappyBird.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class Cocos2dxActivity extends NativeActivity{
-
+	private AdView adView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
-		//For supports translucency
-		
-		//1.change "attribs" in cocos\2d\platform\android\nativeactivity.cpp
-		/*const EGLint attribs[] = {
-	            EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-	            EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,  
-	            //EGL_BLUE_SIZE, 5,   -->delete 
-	            //EGL_GREEN_SIZE, 6,  -->delete 
-	            //EGL_RED_SIZE, 5,    -->delete 
-	            EGL_BUFFER_SIZE, 32,  //-->new field
-	            EGL_DEPTH_SIZE, 16,
-	            EGL_STENCIL_SIZE, 8,
-	            EGL_NONE
-	    };*/
-		
-		//2.Set the format of window
-		// getWindow().setFormat(PixelFormat.TRANSLUCENT);
-		
+		setupAds(); 		
 	}
+	
+	private void setupAds()  
+    {  
+        LinearLayout layout = new LinearLayout(this);  
+        layout.setOrientation(LinearLayout.VERTICAL);  
+        addContentView(layout, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));  
+        adView = new AdView(this); 
+	    adView.setAdSize(AdSize.BANNER);
+	    adView.setAdUnitId("a15289ac58b83d3");
+        
+        layout.addView(adView);  
+        
+	    AdRequest adRequest = new AdRequest.Builder()
+        .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+        .addTestDevice("014E05DF0400B016")
+        .build();
+	    
+        adView.loadAd(adRequest);  
+    }   
 }
